4e050de260f7db6085f0153be0ec61bdcc9678ff
Dr-Aniekan-Udo <aniekanetimudo@gmail.com>
2022-09-09 22:14:32 -0700

    task 0,8,9  fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-
 /**
- * main - prints all numbers of base 16 in lowercase,
- * followed by a new line
+ * main - prints all possoble combinations of single-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	char ch;
 
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-	}
-	for (ch = 'a'; ch <= 'f'; ch++)
-	{
-		putchar(ch);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
