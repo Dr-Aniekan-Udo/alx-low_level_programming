25ec999d7cefc91bffb2055b6a294722a23a93d0
Dr-Aniekan-Udo <aniekanetimudo@gmail.com>
2022-09-22 22:08:36 -0700

    rewrite 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,25 @@
 #include "main.h"
+#include "strlen.c"
 /**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
+ * _strncat - concatenates n bytes from src string to dest string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy from src string
+ * Return: Returns a pointer to dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int count = 0, count2 = 0;
+	char *dest_head = dest;
 
-	while (*(dest + count) != '\0')
+	n = (n > _strlen(src)) ? _strlen(src) : n;
+	while (*dest != '\0')
+		dest++; /* advance to the end of dest */
+	while (n-- > 0)
 	{
-		count++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
-	return (dest);
+	*dest = '\0'; /* inserting null terminator */
+	return (dest_head);
 }
